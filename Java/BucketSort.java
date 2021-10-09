3347b525058de66f8fb4c3a42ba255f42d146929
mainkarakshay7 <55385886+mainkarakshay7@users.noreply.github.com>
2021-10-09 11:19:02 +0530

    Added bucket sort

--- Java/BucketSort.java
+++ Java/BucketSort.java
@@ -1,60 +1,60 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+// Java program to sort an array
+// using bucket sort
+import java.util.*;
 import java.util.Collections;
-import java.util.List;
-import java.util.Random;
- 
-class BucketSort 
-{
-    static int[] bucketSort(int[] sequence, int maxValue) 
-    {
-        // Bucket Sort
-        int[] Bucket = new int[maxValue + 1];
-        int[] sortedSequence = new int[sequence.length];
- 
-        for (int i = 0; i < sequence.length; i++)
-            Bucket[sequence[i]]++;
- 
-        int outPos = 0;
-        for (int i = 0; i < Bucket.length; i++)
-            for (int j = 0; j < Bucket[i]; j++)
-                sortedSequence[outPos++] = i;
- 
-        return sortedSequence;
-    }
- 
-    static void printSequence(int[] sortedSequence) 
-    {
-        for (int i = 0; i < sortedSequence.length; i++)
-            System.out.print(sortedSequence[i] + " ");
-    }
- 
-    static int maxValue(int[] sequence) 
-    {
-        int maxValue = 0;
-        for (int i = 0; i < sequence.length; i++)
-            if (sequence[i] > maxValue)
-                maxValue = sequence[i];
-        return maxValue;
-    }
- 
-    public static void main(String args[]) 
-    {
-        System.out.println("Sorting of randomly generated numbers using BUCKET SORT");
-        Random random = new Random();
-        int N = 20;
-        int[] sequence = new int[N];
- 
-        for (int i = 0; i < N; i++)
-            sequence[i] = Math.abs(random.nextInt(100));
- 
-        int maxValue = maxValue(sequence);
- 
-        System.out.println("\nOriginal Sequence: ");
-        printSequence(sequence);
- 
-        System.out.println("\nSorted Sequence: ");
-        printSequence(bucketSort(sequence, maxValue));
-    }
 
+class GFG {
+
+	// Function to sort arr[] of size n
+	// using bucket sort
+	static void bucketSort(float arr[], int n)
+	{
+		if (n <= 0)
+			return;
+
+		// 1) Create n empty buckets
+		@SuppressWarnings("unchecked")
+		Vector<Float>[] buckets = new Vector[n];
+
+		for (int i = 0; i < n; i++) {
+			buckets[i] = new Vector<Float>();
+		}
+
+		// 2) Put array elements in different buckets
+		for (int i = 0; i < n; i++) {
+			float idx = arr[i] * n;
+			buckets[(int)idx].add(arr[i]);
+		}
+
+		// 3) Sort individual buckets
+		for (int i = 0; i < n; i++) {
+			Collections.sort(buckets[i]);
+		}
+
+		// 4) Concatenate all buckets into arr[]
+		int index = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < buckets[i].size(); j++) {
+				arr[index++] = buckets[i].get(j);
+			}
+		}
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		float arr[] = { (float)0.897, (float)0.565,
+						(float)0.656, (float)0.1234,
+						(float)0.665, (float)0.3434 };
+
+		int n = arr.length;
+		bucketSort(arr, n);
+
+		System.out.println("Sorted array is ");
+		for (float el : arr) {
+			System.out.print(el + " ");
+		}
+	}
 }
+
+// This code is contributed by Himangshu Shekhar Jha
